93131137f743bf7a492621b04be7d1a20a59a5c7
Syrym <60620893+oblist7@users.noreply.github.com>
2022-09-06 15:28:46 +0600

    minor

--- jiberu/src/setupProxy.js
+++ jiberu/src/setupProxy.js
@@ -6,7 +6,7 @@ const context = [
 
 module.exports = function (app) {
     const appProxy = createProxyMiddleware(context, {
-        target: 'https://localhost:7295',
+        target: 'https://localhost:8443',
         secure: false
     });
 
